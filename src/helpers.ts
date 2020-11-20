b5ea9a726cce1532c875be803a3b177b1fab8757
Rodolfo Santos <rodolfodossantos29@gmail.com>
2020-11-20 19:16:01 -0300

    refatorando linhas de código

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,9 +1,7 @@
 export function serialize(obj: any) {
   let queryString: string = '';
   for ( const key in obj ) {
-    if (key !== null) {
-      queryString += `&${key}=${obj[key]}`;
-    }
+    if (key !== null) { queryString += `&${key}=${obj[key]}`; }
   }
   return queryString;
 }
